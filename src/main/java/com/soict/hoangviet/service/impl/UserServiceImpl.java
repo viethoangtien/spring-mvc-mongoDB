acaa6696040a639c836f076873e4566520ce3621
HoangVietSE <viethoangtien@gmail.com>
2019-12-15 23:55:57 +0700

    Fix login mongodb

--- src/main/java/com/soict/hoangviet/service/impl/UserServiceImpl.java
+++ src/main/java/com/soict/hoangviet/service/impl/UserServiceImpl.java
@@ -1,13 +1,19 @@
 package com.soict.hoangviet.service.impl;
 
+import com.soict.hoangviet.dao.IUserDAO;
 import com.soict.hoangviet.model.UserModel;
 import com.soict.hoangviet.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private IUserDAO<UserModel> userDAO;
+
     @Override
     public UserModel findOneByUsernameAndStatus(String username, int status) {
-        return null;
+        return userDAO.findOneByUsernameAndStatus(username,status);
     }
 }
